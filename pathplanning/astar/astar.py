b9680577001780af48b86753eed9ce29a198fab3
Zihao-Felix-Zhou <2562586184@qq.com>
2025-07-08 23:48:14 +0800

    (add): support 3D path planning of drone

---
+++ pathplanning/astar/astar.py
@@ -0,0 +1,98 @@
+from queue import PriorityQueue
+from utils import config
+from utils.util_function import euclidean_distance_3d
+
+
+"""
+Drone 3D path planning using A* algorithm
+
+NOTE: if the distance between the starting point and the end point is too long, then the grid resolution
+should be set large to shorten the running time
+
+References:
+[1] https://www.redblobgames.com/pathfinding/a-star/introduction.html
+[2] https://github.com/yy1115/A-algorithm-3D-path-planning/
+
+Author: Zihao Zhou, eezihaozhou@gmail.com
+Created at: 2025/7/8
+"""
+
+def get_valid_neighbor_pos(current_pos, grid):
+    re = config.GRID_RESOLUTION
+
+    neighbors = []
+    x, y, z = current_pos[0], current_pos[1], current_pos[2]
+
+    directions = [
+        (-re, 0, 0), (re, 0, 0),
+        (0, -re, 0), (0, re, 0),
+        (0, 0, -re), (0, 0, re),
+        (-re, -re, 0), (-re, re, 0),
+        (re, -re, 0), (re, re, 0),
+        (-re, 0, -re), (-re, 0, re),
+        (re, 0, -re), (re, 0, re),
+        (0, -re, -re), (0, -re, re),
+        (0, re, -re), (0, re, re),
+        (-re, -re, -re), (-re, -re, re),
+        (-re, re, -re), (-re, re, re),
+        (re, -re, -re), (re, -re, re),
+        (re, re, -re), (re, re, re)
+    ]
+
+    for dx, dy, dz in directions:
+        next_x = x + dx
+        next_y = y + dy
+        next_z = z + dz
+        if 0 <= next_x < config.MAP_LENGTH and 0 <= next_y < config.MAP_WIDTH and 0 <= next_z < config.MAP_HEIGHT:
+            if grid[int(next_x/config.GRID_RESOLUTION),
+                    int(next_y/config.GRID_RESOLUTION),
+                    int(next_z/config.GRID_RESOLUTION)] == 0:
+                neighbors.append((next_x, next_y, next_z))
+
+    return neighbors
+
+
+def a_star_3d(start_pos, end_pos, grid):
+    """
+    Implementation of the A* algorithm for 3D path planning
+
+    Args:
+        start_pos: starting position of the drone, tuple(list)
+        end_pos: ending position of the drone, tuple(list)
+        grid: the 3D mesh
+
+    Returns: the path (if it exists)
+
+    """
+
+    frontier = PriorityQueue()  # used to store the points to be traversed
+    frontier.put((0, start_pos))
+    came_from = dict()
+    came_from[start_pos] = None
+
+    cost_so_far = dict()
+    cost_so_far[start_pos] = 0
+
+    while not frontier.empty():
+        _, current_pos = frontier.get()
+
+        if current_pos == end_pos:
+            current_pos = end_pos
+            path = []
+            while current_pos != start_pos:
+                path.append(current_pos)
+                current_pos = came_from[current_pos]
+            path.append(start_pos)
+            path.reverse()
+            return path
+
+        for neighbor_pos in get_valid_neighbor_pos(current_pos, grid):
+            new_cost = cost_so_far[current_pos] + euclidean_distance_3d(current_pos, neighbor_pos)
+
+            if neighbor_pos not in cost_so_far or new_cost < cost_so_far[neighbor_pos]:
+                cost_so_far[neighbor_pos] = new_cost
+                priority = new_cost + euclidean_distance_3d(neighbor_pos, end_pos)
+                frontier.put((priority, neighbor_pos))
+                came_from[neighbor_pos] = current_pos
+
+    return None
